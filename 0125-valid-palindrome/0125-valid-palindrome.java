9081e99344dd4bfc9ccdb3b6c599d6f1ea12a5a4
Ankur Kumar Mishra <ankur13189@gmail.com>
2026-09-01 19:50:31 +0530

    Time: 2 ms (99.3%), Space: 43.8 MB (98.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        /*
         s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         String s_rev = new StringBuilder(s).reverse().toString();
         int i=0;
@@ -7,6 +8,25 @@ class Solution {
             if(s.charAt(i) != s_rev.charAt(i)) return false;
             i=i+1;
         }
-        return true;   
+        return true; 
+        */
+        int i=0, j=s.length()-1;
+        while(i<j){
+            char left=s.charAt(i);
+            char right=s.charAt(j);
+            if(!Character.isLetterOrDigit(left)){
+                i++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(right)){
+                j--;
+                continue;
+            }
+            if(Character.toLowerCase(left) != Character.toLowerCase(right)) return false;
+            i++;
+            j--;
+        } 
+        return true;
+
     }
 }
